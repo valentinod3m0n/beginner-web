dbc08b5d3a21979989350e32242ba9a43e98b897
valentinod3m0n <therolus@gmail.com>
2018-10-21 18:55:48 +0200

    Added a form

---
+++ process.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Respuesta</title>
+</head>
+<body>
+  <main>
+    <section>
+      <p>
+        <?php
+          print('Tu nombre es: ' .$GET['nombre']);
+        ?>
+      </p>
+    <section>
+  </main>
+</body>
+</html>
